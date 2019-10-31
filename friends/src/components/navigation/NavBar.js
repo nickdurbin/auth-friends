eaab58132dece5d7b3d91864a421528568e69b7b
ndurbin <nick.durbin@gmail.com>
2019-10-31 05:27:06 -0400

    Did some initial styling to the navbar.

--- friends/src/components/navigation/NavBar.js
+++ friends/src/components/navigation/NavBar.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import HomePage from '../home/HomePage';
-import LoginForm from '../login/LoginForm';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
+  padding: 2%;
   background: black;
   color: white;
 `;
 
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+  width: 15%;
+`
+
 const Logo = styled.h1`
-  font-size: 2rem;
+  font-size: 1.6rem;
   font-weight: 600;
   color: white;
 `;
@@ -22,10 +28,14 @@ function NavBar() {
   return (
     <Nav className='navContainer'>
       <Logo>Friendz-Own</Logo>
-      <div className='navLinks'>
-        <NavLink exact path='/' component={HomePage} />
-        <NavLink path='/login' component={LoginForm} />
-      </div>
+      <NavLinks className='navLinks'>
+        <NavLink to='/'>
+          Home
+        </NavLink>
+        <NavLink to='/login'>
+          Login
+        </NavLink>
+      </NavLinks>
     </Nav>
   )
 }
