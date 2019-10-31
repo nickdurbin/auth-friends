607e0958acc1f96a0cf733d2de681ef4f27e5e65
ndurbin <nick.durbin@gmail.com>
2019-10-31 05:07:53 -0400

    Added the NavBar component to the App.js.

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NavBar from './components/navigation/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
