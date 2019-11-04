f698c873ba73d02dea17f67fc40b9b07c11fe8db
nick.durbin <nick.durbin@gmail.com>
2019-11-04 17:58:04 -0500

    Going to add the rest of the functionality to the app.

---
+++ friends/src/components/friends/FriendForm.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function FriendForm() {
+  const [userCredentials, setUserCredentials] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChanges = e => {
+    setUserCredentials({
+      ...userCredentials,
+      [e.target.name]: e.target.value
+    })
+  };
+
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axiosWithoutAuth()
+      .post('/api/login', userCredentials)
+      .then(res => {
+        console.log(res.data)
+        localStorage.setItem("token", res.data.payload);
+        props.history.push('/home');
+      })
+      .catch(err => {
+        return err.response
+      })
+    resetForm();
+  }
+
+  const resetForm = () => {
+    setUserCredentials({
+      username: '',
+      password: ''
+    })
+  }
+
+  return (
+    <div className='formContainer'>
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <hr />
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={userCredentials.username}
+          onChange={handleChanges}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={userCredentials.password}
+          onChange={handleChanges}
+        />
+        <button className="addBtn btn" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default FriendForm;
